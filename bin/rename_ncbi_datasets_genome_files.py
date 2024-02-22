fb4bb7ebc0d15723366c0ee85f7a269b83553507
chasemc <18691127+chasemc@users.noreply.github.com>
2024-02-22 07:21:58 -0600

    fix: ncbi datasets renaming
    Only files with Assembly metadata tags were parsed. Now the filenames are changed as well using the json info from the download API and double checked with the directory name. SGPY uses the filename if there's no assembly tag so this should solve the issue

--- bin/rename_ncbi_datasets_genome_files.py
+++ bin/rename_ncbi_datasets_genome_files.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
 
-import gzip
 from pathlib import Path
+import json
+
+# When downloaded from NCBI, the genome files are named genomic.gbff.gz and placed in a folder named after the accession number
+# This script renames the files to the accession number using the dataset_catalog.json file
+
+# ├── ncbi_dataset
+# │   ├── data
+# │   │   ├── assembly_data_report.jsonl
+# │   │   ├── dataset_catalog.json
+# │   │   ├── GCA_003248315.1
+# │   │   │   └── genomic.gbff.gz
+# │   │   └── GCF_000506385.1
+# │   │       └── genomic.gbff.gz
+# │   └── fetch.txt
+# └── README.md
+
+# TO
+
+# ├── ncbi_dataset
+# │   ├── data
+# │   │   ├── assembly_data_report.jsonl
+# │   │   ├── dataset_catalog.json
+# │   │   ├── GCA_003248315.1
+# │   │   │   └── GCA_003248315.1.gbff.gz
+# │   │   └── GCF_000506385.1
+# │   │       └── GCF_000506385.1.gbff.gz
+# │   └── fetch.txt
+# └── README.md
 
 
-for input_path in Path(".").glob("**/genomic.gbff.gz"):
-    assembly = None
-    with gzip.open(input_path, "rt") as f:
-        for line in f:
-            if line.startswith("            Assembly: "):
-                assembly = line.strip().split(": ")[1].strip()
-                break
-    filename = input_path.name
-    if assembly:
-        # change file name to include assembly
-        input_path.replace(input_path.with_name(f"{assembly}.gbff.gz"))
+current_gbff_paths = list(Path(".").glob(f"**/genomic.gbff.gz"))
+lookupdict = {str(Path(i.parents[0].name, i.name)).removesuffix('.gz'):i for i in current_gbff_paths}
+
+for input_path in Path(".").glob("**/dataset_catalog.json"):
+    with open(input_path) as f:
+        data = json.load(f)
+        for i in data["assemblies"]:
+            if "accession" in i:
+                for ii in i["files"]:
+                    if ii['filePath'].endswith('genomic.gbff'):
+                       lookupdict[ii['filePath']].rename(Path(lookupdict[ii['filePath']]).with_name(f"{i['accession']}.gbff.gz"))
+
+
+for i in Path(".").glob(f"**/*.gbff.gz"):
+    if not i.parents[0].name == i.name.removesuffix(".gbff.gz"):
+        print("Assumption is that NCBI dataset files are in a folder named after the accession number\nand files themselves are renamed after teh accession.")
+        print(f"Expected {i.parents[0].name} but got {i.name.removesuffix('.gbff.gz')}")
+        raise ValueError(f"File {i} is not in the correct folder")
+
+
+

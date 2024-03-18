3fe3f9dbe4970134abe58f381ce241994dc65dd6
chasemc <18691127+chasemc@users.noreply.github.com>
2024-03-18 08:24:27 -0500

    temp

--- dockerfiles/make_docker.sh
+++ dockerfiles/make_docker.sh
@@ -30,9 +30,13 @@ while [ "$2" != "" ]; do
     shift
 done
 
-# pushd antismash
-# docker build . -t chasemc2/sgnf-antismash:0.4.4
-# popd
+pushd antismash
+docker build . -t chasemc2/sgnf-antismash:0.4.4
+popd
+
+pushd sgpy/sgpy_base
+docker build . -t chasemc2/sgpy-base:0.4.4
+popd
 
 pushd sgpy
 docker build . -t chasemc2/sgnf-sgpy:0.4.4
@@ -58,3 +62,8 @@ if [[ $UPLOAD == true ]]; then
     docker push chasemc2/sgnf-hmmer:0.4.4
     docker push chasemc2/sgnf-hmmer_plus:0.4.4
 fi
+
+
+
+
+docker build --target build -t hello .
